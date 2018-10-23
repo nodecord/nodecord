5e783200e42792054aa26012fa1bf170224ea327
Tony <twatt74@eq.edu.au>
2018-10-23 11:16:45 +1000

    Partial start on embed creator

--- src/util/Message/classes/MessageEmbed.js
+++ src/util/Message/classes/MessageEmbed.js
@@ -1,5 +1,12 @@
-module.exports = class MessageEmbed extends {} {
+module.exports = class MessageEmbed {
     constructor() {
         return;
+    }
+
+    setTitle(str) {
+        if (!str || str == '') throw new TypeError(`Message embed titles cannot be empty`);
+
+        this.title = `${str}`;
+        return this;
     }
 }
