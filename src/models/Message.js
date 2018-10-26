421bd0e819aaa0c47336a5c206e0d95e033e08d0
Tony <twatt74@eq.edu.au>
2018-10-26 11:32:22 +1000

    Message class changes to make it better 1/2

--- src/models/Message.js
+++ src/models/Message.js
@@ -3,16 +3,23 @@ const Guild = require('./Guild'), TextChannel = require('./TextChannel');
 
 module.exports = class Message {
     constructor(obj, addons, client) {
-        const blacklisted = {
-            'guild_id': true,
-            'channel_id': true,
-        };
-
-        for (const [key, value] of Object.entries(obj)) {
-            if (!blacklisted.hasOwnProperty(key)) this[key] = value;
+        this.id = obj.id;
+        if (obj.webhook_id) {
+            this.author = new WebhookUser();
+        } else {
+            this.author = new User();
+            this.member = new GuildMember();
         }
-
-        this.cleanedContent = MessageUtil.cleanMessage(obj.content);
+        this.content = obj.content;
+        this.cleaned = MessageUtil.cleanMessage(obj.content);
+        this.createdAt = new Date(obj.timestamp);
+        if (obj.edited_timestamp != null) this.editedAt = new Date(obj.edited_timestamp);
+        this.tts = obj.tts;
+        this.pinned = obj.pinned;
+        this.mentions = obj.mentions;
+        this.attachments = obj.attachments;
+        this.embeds = obj.embeds;
+        this.reactions = obj.reactions;
         this.guild = new Guild(addons.guild);
         this.channel = new TextChannel(addons.channel, client);
     }
