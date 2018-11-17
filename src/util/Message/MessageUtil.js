a8cf3f36e3d983cc197f124935ecd97fcb179bc2
Tony <twatt74@eq.edu.au>
2018-11-18 09:48:42 +1000

    Add message cleaner finally

--- src/util/Message/MessageUtil.js
+++ src/util/Message/MessageUtil.js
@@ -1,4 +1,9 @@
+const clean = (text) => {
+    if (typeof(text) == 'string') return text.replace(/[`@]/g, '$&\u200b');
+    else return text;
+};
+
 exports.cleanMessage = async (content) => {
-    let cleaned = content;
+    const cleaned = clean(content);
     return cleaned;
 }
