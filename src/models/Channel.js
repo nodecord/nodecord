bd3f6683b5c6c58e2bb53b7212361a1e52bb45d9
Tony <twatt74@eq.edu.au>
2018-11-18 09:48:52 +1000

    Channel types make more sense now

--- src/models/Channel.js
+++ src/models/Channel.js
@@ -10,7 +10,7 @@ module.exports = class Channel {
             case 0: this.type = 'text'; break;
             case 1: this.type = 'dm'; break;
             case 2: this.type = 'voice'; break;
-            case 3: this.type = 'dm_group'; break;
+            case 3: this.type = 'group'; break;
             case 4: this.type = 'category'; break;
         }
     }
