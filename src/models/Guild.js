b05c5421b0c8b34245356e7f1388122f27e38ce1
ThatTonybo <twatt74@eq.edu.au>
2020-04-20 14:14:11 +1000

    Bye bye current generation files

--- src/models/Guild.js
+++
@@ -1,7 +0,0 @@
-module.exports = class Guild {
-    constructor(obj) {
-        for (const [key, value] of Object.entries(obj)) {
-            this[key] = value;
-        }
-    }
-}
--- src/util/Message/MessageUtil.js
+++
@@ -1,9 +0,0 @@
-const clean = (text) => {
-    if (typeof(text) == 'string') return text.replace(/[`@]/g, '$&\u200b');
-    else return text;
-};
-
-exports.cleanMessage = async (content) => {
-    const cleaned = clean(content);
-    return cleaned;
-}
